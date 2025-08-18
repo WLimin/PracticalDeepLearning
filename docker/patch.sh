d0598d5b2365bffa6b656ec983bf7fe13473f520
wang <w.limin@qq.com>
2025-08-18 12:30:22 +0800

    删除无用注释

---
+++ docker/patch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# 目标：容易删除容器且不再次下载
+# robust_downloader.downloader - INFO - Downloading https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.bin to lid.176.bin (125.2M)
+#sed -i -e 's#cache_dir=.*fast_langdetect"#cache_dir="models/gpt_sovits_v4/fast_langdetect"#g' /app/text/LangSegmenter/langsegmenter.py
+
+echo "done."
